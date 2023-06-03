34ed588455ee73b6014bb528c42246cede94d170
Алексей Труничев <trunichev.aleksei@yandex.ru>
2023-06-03 10:53:36 +0300

    3

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -15,4 +15,6 @@ router.register(r'mycats', LightCatViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
